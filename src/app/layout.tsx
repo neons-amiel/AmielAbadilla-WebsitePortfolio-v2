44e20b317c3e463407895c7f15c4ac24d030b072
neons-amiel <robertamiel.abadilla@gmail.com>
2026-05-06 21:27:11 +0800

    1. added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
 import { Lexend, Montserrat } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/next"; // 
 import Header from "@/src/components/header";
 import Footer from "@/src/components/footer";
 import ParticlesSnow from "@/src/components/particles-snow";
 
 const lexend = Lexend({
   subsets: ['latin'],
-  variable: '--font-lexend', // Define a CSS variable name
+  variable: '--font-lexend', 
   display: 'swap',
 });
 
-// Configure Montserrat (Optional)
 const montserrat = Montserrat({
   subsets: ['latin'],
   variable: '--font-montserrat',
@@ -37,6 +36,7 @@ export default function RootLayout({
       <body className="min-h-dvh bg-zinc-950 text-zinc-50">
         <ParticlesSnow className="pointer-events-none fixed inset-0 -z-10" />
         <Header />
+        
         <div className="mx-auto flex min-h-dvh w-full max-w-6xl flex-col px-4 sm:px-6 lg:px-8">
           {/* Offset for fixed header */}
           <div className="h-16" />
@@ -44,8 +44,11 @@ export default function RootLayout({
             <div className="relative z-10">{children}</div>
           </div>
         </div>
+        
         <Footer/>
         
+        {/* VERCEL ANALYTICS COMPONENT */}
+        <Analytics />
         
       </body>
     </html>
